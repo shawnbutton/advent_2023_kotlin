487e3e49023e6d1c1720068b3976d8e0f5ac00b7
shawn <shawnbutton@gmail.com>
2023-12-01 18:40:50 -0500

    get the digits from a line

--- src/main/kotlin/com/shawnbutton/advent2023/Advent.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/Advent.kt
@@ -1,8 +1,20 @@
 package com.shawnbutton.advent2023
 
+import kotlin.math.log
+
 class Advent {
     fun sum(a: Int, b: Int): Int {
         return a + b
+    }
+
+    fun calcDigits(line: String): Int {
+        val digits  =  line
+            .toCharArray()
+            .filter { it.isDigit() }
+
+        val rightDigits = "" + digits.first() + digits.last()
+
+        return rightDigits.toInt()
     }
 
 }
--- src/test/kotlin/com/shawnbutton/advent2023/AdventTest.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/AdventTest.kt
@@ -12,4 +12,17 @@ class AdventTest {
 
         assertEquals(3, result)
     }
+
+    @Test
+    fun should_calculate_value_of_line_from_digits() {
+        val advent = Advent()
+
+        assertEquals(13, advent.calcDigits("x123x"))
+        assertEquals(24, advent.calcDigits("234x"))
+        assertEquals(35, advent.calcDigits("x345"))
+        assertEquals(46, advent.calcDigits("456"))
+        assertEquals(77, advent.calcDigits("x7x"))
+        assertEquals(88, advent.calcDigits("8"))
+    }
+
 }

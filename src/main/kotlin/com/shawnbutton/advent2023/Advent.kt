82c564fe01d8a3b44c8c44df0647dc15c52b0757
shawn <shawnbutton@gmail.com>
2023-12-01 20:16:36 -0500

    load file

--- src/main/kotlin/com/shawnbutton/advent2023/Advent.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/Advent.kt
@@ -1,20 +1,22 @@
 package com.shawnbutton.advent2023
 
-import kotlin.math.log
+import java.io.File
 
-class Advent {
-    fun sum(a: Int, b: Int): Int {
-        return a + b
-    }
+fun calcDigits(line: String): Int {
+    val digits = line
+        .toCharArray()
+        .filter { it.isDigit() }
 
-    fun calcDigits(line: String): Int {
-        val digits  =  line
-            .toCharArray()
-            .filter { it.isDigit() }
+    val rightDigits = "" + digits.first() + digits.last()
 
-        val rightDigits = "" + digits.first() + digits.last()
+    return rightDigits.toInt()
+}
 
-        return rightDigits.toInt()
-    }
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
 
+fun loadFle(fileName: String): List<String> {
+    val contents = Any::class::class.java.getResource(fileName)!!.readText()
+    return contents.split("\n").dropLast(1)
 }
--- src/test/kotlin/com/shawnbutton/advent2023/AdventTest.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/AdventTest.kt
@@ -2,27 +2,31 @@ package com.shawnbutton.advent2023
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 class AdventTest {
     @Test
     fun should_calculate_sum() {
-        val advent = Advent()
-
-        val result = advent.sum(1, 2)
+        val result = sum(1, 2)
 
         assertEquals(3, result)
     }
 
     @Test
     fun should_calculate_value_of_line_from_digits() {
-        val advent = Advent()
-
-        assertEquals(13, advent.calcDigits("x123x"))
-        assertEquals(24, advent.calcDigits("234x"))
-        assertEquals(35, advent.calcDigits("x345"))
-        assertEquals(46, advent.calcDigits("456"))
-        assertEquals(77, advent.calcDigits("x7x"))
-        assertEquals(88, advent.calcDigits("8"))
+        assertEquals(13, calcDigits("x123x"))
+        assertEquals(24, calcDigits("234x"))
+        assertEquals(35, calcDigits("x345"))
+        assertEquals(46, calcDigits("456"))
+        assertEquals(77, calcDigits("x7x"))
+        assertEquals(88, calcDigits("8"))
     }
+
+    @Test
+    fun should_load_file() {
+        assertEquals(listOf("first line", "second line"), loadFle("/sample.txt"))
+    }
+
+
 
 }

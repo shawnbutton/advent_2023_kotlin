6996e073cac4ef5d331c6a44f258bbd72a93b92c
shawn <shawnbutton@gmail.com>
2023-12-04 13:31:53 -0500

    parse line

--- src/main/kotlin/com/shawnbutton/advent2023/day04/Day04.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day04/Day04.kt
@@ -2,15 +2,18 @@ package com.shawnbutton.advent2023.day04
 
 import com.shawnbutton.advent2023.loadFile
 
-data class Part(val value: Int, val start: Int, val end: Int)
+data class Card(val cardNumber: Int, val winning: List<Int>, val yours: List<Int>)
 
-fun main() {
-    val lines = loadFile("/day04.txt")
+fun parseLine(line: String): Any {
+    val cardNumber = line.substringAfter("Card ").substringBefore(":").toInt()
+    val winning = line.substringAfter(": ").substringBefore(" | ").split(" ").map { it.toInt() }
+    val yours = line.substringAfter(" | ").split(" ")
+        .filter(String::isNotEmpty)
+        .map { it.toInt() }
 
-    print(doPart1(lines))
-    print("\n")
-    print(doPart2(lines))
+    return Card(cardNumber, winning, yours)
 }
+
 
 fun doPart1(lines: List<String>): Int {
     return -1
@@ -23,3 +26,10 @@ fun doPart2(lines: List<String>): Int {
 
 }
 
+fun main() {
+    val lines = loadFile("/day04.txt")
+
+    print(doPart1(lines))
+    print("\n")
+    print(doPart2(lines))
+}
--- src/test/kotlin/com/shawnbutton/advent2023/day04/Day04Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day04/Day04Test.kt
@@ -6,19 +6,28 @@ import org.junit.jupiter.api.Test
 
 class Day04Test {
 
+    val line0 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    val line1 = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
+    val line2 = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
+    val line3 = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"
+    val line4 = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
+    val line5 = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+
+    val lines = listOf(line0, line1, line2, line3, line4, line5)
 
     @Test
-    fun `test part 1`() {
-        val lines = loadFile("/day04.txt")
+    fun `should parse the line`() {
+        val expected = Card(
+            1,
+            listOf(41, 48, 83, 86, 17),
+            listOf(83, 86, 6, 31, 17, 9, 48, 53)
+        )
 
-        assertEquals(0, doPart1(lines))
+        val response = parseLine(line0)
+
+        assertEquals(expected, response)
+
     }
 
-    @Test
-    fun `test part 2`() {
-        val lines = loadFile("/day04.txt")
-
-        assertEquals(0, doPart2(lines))
-    }
 
 }

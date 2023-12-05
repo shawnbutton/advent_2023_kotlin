f546e0e25d60faea4f7d9e254b66dc1ced7b654b
shawn <shawnbutton@gmail.com>
2023-12-04 19:54:13 -0500

    ready for day 5

---
+++ src/main/kotlin/com/shawnbutton/advent2023/day05/Day05.kt
@@ -0,0 +1,22 @@
+package com.shawnbutton.advent2023.day05
+
+import com.shawnbutton.advent2023.loadFile
+import kotlin.math.pow
+
+
+fun doPart1(lines: List<String>): Int {
+    return -1
+}
+
+fun doPart2(lines: List<String>): Int {
+    return -1
+}
+
+
+fun main() {
+    val lines = loadFile("/day04.txt")
+
+    print(doPart1(lines))
+    print("\n")
+    print(doPart2(lines))
+}
---
+++ src/test/kotlin/com/shawnbutton/advent2023/day05/Day05Test.kt
@@ -0,0 +1,9 @@
+package com.shawnbutton.advent2023.day04
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+
+
+}

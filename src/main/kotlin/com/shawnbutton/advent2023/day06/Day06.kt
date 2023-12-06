777a0de1e3cf49ef555b38d9ae30f2635414dc8e
shawn <shawnbutton@gmail.com>
2023-12-06 15:58:36 -0500

    prepare for day 6

---
+++ src/main/kotlin/com/shawnbutton/advent2023/day06/Day06.kt
@@ -0,0 +1,17 @@
+package com.shawnbutton.advent2023.day06
+
+fun doPart1(lines: List<String>): Int {
+    return -1
+}
+
+fun doPart2(lines: List<String>): Int {
+return -1
+}
+
+
+fun main() {
+//    print(doPart1(lines))
+//    print("\n")
+//    print(doPart2(lines))
+//    print("\n")
+}
---
+++ src/test/kotlin/com/shawnbutton/advent2023/day06/Day06Test.kt
@@ -0,0 +1,13 @@
+package com.shawnbutton.advent2023.day06
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+
+    @Test
+    fun `should do something`() {
+        assertEquals(35, 35)
+    }
+
+}

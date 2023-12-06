3f3df7dd2a6a4d37771b9735f86ed72228d5c017
shawn <shawnbutton@gmail.com>
2023-12-05 22:21:28 -0500

    port some helper functions over from typescript

--- src/main/kotlin/com/shawnbutton/advent2023/day05/Day05.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day05/Day05.kt
@@ -1,8 +1,37 @@
 package com.shawnbutton.advent2023.day05
 
 import com.shawnbutton.advent2023.loadFile
-import kotlin.math.pow
 
+data class RangeMap(val sourceFrom: Int, val sourceTo: Int, val destinationOffset: Int)
+
+fun getSeeds(lines: List<String>): List<Int> {
+    val seedLine = lines.find { it.startsWith("seeds: ") };
+    return seedLine!!
+        .split(": ")
+        .get(1)
+        .trim()
+        .split(" ")
+        .map { it.toInt() }
+}
+
+fun makeRange(line: String): RangeMap {
+    val segments = line.split(" ").map { it.toInt() }
+
+    return RangeMap(segments[1], segments[2], segments[0])
+}
+//
+//    transform(range: RangeMap, number: number): number {
+//        return 50;
+//    }
+//
+//    makeRangeMap(sourceStart: number, size: number, destinationStart: number) {
+//        return {
+//            sourceFrom: sourceStart,
+//            sourceTo: sourceStart + size - 1,
+//            destinationOffset: destinationStart - sourceStart
+//        };
+//    }
+//}
 
 fun doPart1(lines: List<String>): Int {
     return -1
--- src/test/kotlin/com/shawnbutton/advent2023/day05/Day05Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day05/Day05Test.kt
@@ -1,9 +1,41 @@
-package com.shawnbutton.advent2023.day04
+package com.shawnbutton.advent2023.day05
 
+import com.shawnbutton.advent2023.loadFile
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day05Test {
+
+    val lines = loadFile("/day05_sample.txt");
+
+    @Test
+    fun `should get planted seeds`() {
+        assertEquals(getSeeds(lines), listOf(79, 14, 55, 13));
+    }
+
+
+    fun `should make range map`() {
+        val range = makeRange("50 98 2");
+
+        assertEquals(range, RangeMap(98, 99, -48));
+    }
+
+
+    /**///
+//            describe('should convert from source to destination', () => {
+//                fun 'should convert when found in range'() {
+//                    val sut = new Advent();
+//
+//                    assertEquals(sut.transform(sut.makeRangeM(), 98)).toEqual(50)
+//                });
+//            });
+//
+//            fun 'should get various structures'() {
+//                // val maps = sut.parseMaps(lines);
+//                // assertEquals(maps).
+//            });
+//        });
+//    });
 
 
 }

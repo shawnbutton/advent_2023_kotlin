5a485fbcc0d86604ea90ed1726956693cb316054
shawn <shawnbutton@gmail.com>
2023-12-06 19:01:21 -0500

    finish day 6 part 1

--- src/main/kotlin/com/shawnbutton/advent2023/day06/Day06.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day06/Day06.kt
@@ -1,17 +1,41 @@
 package com.shawnbutton.advent2023.day06
 
-fun doPart1(lines: List<String>): Int {
-    return -1
+fun Int?.isGreaterThan(other: Int?) =
+    this != null && other != null && this > other
+
+data class Race(val length: Int, val theRecord: Int)
+
+fun waysToWin(race: Race): Int {
+    return (0..race.length)
+        .map { secondOn ->
+            val secondsLeft = race.length - secondOn
+            secondsLeft * secondOn
+        }
+        .filter { it -> it.isGreaterThan(race.theRecord) }
+        .count()
 }
 
-fun doPart2(lines: List<String>): Int {
-return -1
+fun doPart1(races: List<Race>): Int {
+    return races.map(::waysToWin)
+        .reduce { acc, next -> acc * next }
+}
+
+fun doPart2(races: List<Race>): Int {
+    return -1
 }
 
 
 fun main() {
-//    print(doPart1(lines))
-//    print("\n")
+
+    val races = listOf(
+        Race(51, 222),
+        Race(92, 2031),
+        Race(68, 1126),
+        Race(90, 1225)
+    )
+
+    print(doPart1(races))
+    print("\n")
 //    print(doPart2(lines))
 //    print("\n")
 }
--- src/test/kotlin/com/shawnbutton/advent2023/day06/Day06Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day06/Day06Test.kt
@@ -5,9 +5,20 @@ import org.junit.jupiter.api.Test
 
 class Day06Test {
 
+    val race1 = Race(7, 9)
+    val race2 = Race(15, 40)
+    val race3 = Race(30, 200)
+
     @Test
-    fun `should do something`() {
-        assertEquals(35, 35)
+    fun `should count ways to win`() {
+        assertEquals(4, waysToWin(race1))
+        assertEquals(8, waysToWin(race2))
+        assertEquals(9, waysToWin(race3))
+    }
+
+    @Test
+    fun `should do part 1`() {
+        assertEquals(288, doPart1(listOf(race1, race2, race3)))
     }
 
 }

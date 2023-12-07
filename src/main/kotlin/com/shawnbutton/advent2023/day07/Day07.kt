10e057bf3d08fe9e601f9511bf332f967b2c1406
shawn <shawnbutton@gmail.com>
2023-12-07 16:38:29 -0500

    ready for day 7

---
+++ src/main/kotlin/com/shawnbutton/advent2023/day07/Day07.kt
@@ -0,0 +1,19 @@
+package com.shawnbutton.advent2023.day07
+
+import com.shawnbutton.advent2023.day06.Race
+
+fun doPartA(races: List<Race>): Long {
+ return -1L
+}
+
+fun doPartB(race: Race): Long {
+    return -1L
+}
+
+fun main() {
+//    print(doPart1(races))
+    print("\n")
+
+//    print(doPart2(bigRace))
+    print("\n")
+}
---
+++ src/test/kotlin/com/shawnbutton/advent2023/day07/Day07Test.kt
@@ -0,0 +1,7 @@
+package com.shawnbutton.advent2023.day07
+
+class Day07Test {
+
+
+
+}

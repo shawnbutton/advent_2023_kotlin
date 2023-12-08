29a033fb2dcf5fef1d142189cc5bab3e3c19ca27
shawn <shawnbutton@gmail.com>
2023-12-07 22:39:16 -0500

    rank list of hands

--- src/main/kotlin/com/shawnbutton/advent2023/day07/Day07.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day07/Day07.kt
@@ -12,11 +12,27 @@ enum class HandValue {
     FiveOfAKind
 }
 
+val rankMap = mapOf(
+    "A" to 14,
+    "K" to 13,
+    "Q" to 12,
+    "J" to 11,
+    "T" to 10,
+    "9" to 9,
+    "8" to 8,
+    "7" to 7,
+    "6" to 6,
+    "5" to 5,
+    "4" to 4,
+    "3" to 3,
+    "2" to 2
+)
+
 fun parseHand(line: String): List<String> {
     return line.substringBefore(" ").toList().map { it.toString() }
 }
 
-fun valueHand(hand: List<String>): HandValue {
+fun rankHand(hand: List<String>): HandValue {
     val groupedByCard = hand.groupBy { it }
 
     val numPairs = groupedByCard.filter { it.value.size == 2 }.size
@@ -36,6 +52,20 @@ fun valueHand(hand: List<String>): HandValue {
         onePair  -> HandValue.Pair
         else -> HandValue.HighCard
     }
+}
+
+fun largestWithinRank(hand1: List<String>, hand2: List<String>): Int {
+    hand1
+        .zip(hand2)
+        .first { it.first != it.second }
+        .let { (card1, card2) ->
+            return if (getCardValue(card1) > getCardValue(card2)) 0 else 1
+        }
+}
+
+private fun getCardValue(card: String): Int {
+    print("card: $card")
+    return rankMap.get(card)!!
 }
 
 fun doPartA(races: List<Race>): Long {
--- src/test/kotlin/com/shawnbutton/advent2023/day07/Day07Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day07/Day07Test.kt
@@ -6,11 +6,11 @@ import kotlin.test.assertContentEquals
 
 class Day07Test {
 
-    val line1 = "32T3K 765"
-    val line2 = "T55J5 684"
-    val line3 = "KK677 28"
-    val line4 = "KTJJT 220"
-    val line5 = "QQQJA 483"
+    val line1 = "32T3K 765" // pair
+    val line2 = "T55J5 684" // three of a kind
+    val line3 = "KK677 28" // two pair
+    val line4 = "KTJJT 220" // two pair
+    val line5 = "QQQJA 483" // three of a kind
 
     val lines = listOf(line1, line2, line3, line4, line5)
 
@@ -21,42 +21,80 @@ class Day07Test {
     }
 
     fun `can find a pair`() {
-        val result = valueHand(listOf("A", "A", "2", "3", "4"))
+        val result = rankHand(listOf("A", "A", "2", "3", "4"))
         assertEquals(result, HandValue.Pair)
     }
 
     fun `can find two pair`() {
-        val result = valueHand(listOf("A", "A", "2", "2", "3"))
+        val result = rankHand(listOf("A", "A", "2", "2", "3"))
         assertEquals(result, HandValue.TwoPair)
     }
 
     @Test
     fun `can find three of a kind`() {
-        val result = valueHand(listOf("A", "A", "A", "2", "3"))
+        val result = rankHand(listOf("A", "A", "A", "2", "3"))
         assertEquals(result, HandValue.ThreeOfAKind)
     }
 
     @Test
     fun `can find full house`() {
-        val result = valueHand(listOf("A", "A", "A", "2", "2"))
+        val result = rankHand(listOf("A", "A", "A", "2", "2"))
         assertEquals(result, HandValue.FullHouse)
     }
 
     @Test
     fun `can find four of a kind`() {
-        val result = valueHand(listOf("A", "A", "A", "A", "2"))
+        val result = rankHand(listOf("A", "A", "A", "A", "2"))
         assertEquals(result, HandValue.FourOfAKind)
     }
 
     @Test
     fun `can find five of a kind`() {
-        val result = valueHand(listOf("A", "A", "A", "A", "A"))
+        val result = rankHand(listOf("A", "A", "A", "A", "A"))
         assertEquals(result, HandValue.FiveOfAKind)
     }
 
     @Test
     fun `can find high card`() {
-        val result = valueHand(listOf("A", "2", "3", "4", "5"))
+        val result = rankHand(listOf("A", "2", "3", "4", "5"))
         assertEquals(result, HandValue.HighCard)
     }
+
+    @Test
+    fun `same rank - highest first card wins`() {
+        val hand1 = listOf("A")
+        val hand2 = listOf("K")
+
+        assertEquals(0, largestWithinRank(hand1, hand2))
+    }
+
+    @Test
+    fun `same rank - if first same, second card wins`() {
+        val hand1 = listOf("A", "3")
+        val hand2 = listOf("A", "2")
+
+        assertEquals(0, largestWithinRank(hand1, hand2))
+    }
+
+    @Test
+    fun `same rank - third card wins`() {
+        val hand1 = listOf("A", "A", "2")
+        val hand2 = listOf("A", "A", "3")
+
+        assertEquals(1, largestWithinRank(hand1, hand2))
+    }
+
+    @Test
+    fun `rank hands`() {
+        val valuedHands = lines
+            .map { parseHand(it) }
+            .map { rankHand(it) }
+
+        val result = valuedHands
+            .map { it.ordinal }
+            .joinToString("")
+
+        assertEquals("13223", result)
+    }
+
 }

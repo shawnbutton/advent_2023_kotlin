890be0c62627fc0f0916f662bef610c94dcd44c4
shawn <shawnbutton@gmail.com>
2023-12-09 00:24:59 -0500

    extract instructions and directions map

--- src/main/kotlin/com/shawnbutton/advent2023/day08/Day08.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day08/Day08.kt
@@ -2,13 +2,28 @@ package com.shawnbutton.advent2023.day08
 
 import com.shawnbutton.advent2023.loadFile
 
+fun getIntructions(lines: List<String>): String {
+    return lines[0]
+}
+
+
+fun getDirections(lines: List<String>): Map<String, Pair<String, String>> {
+    return lines.slice(2..lines.size - 1).map {
+        val key = it.substringBefore(" = ")
+        val (left, right) = it.substringAfter(" = (").substringBefore(")").split(", ")
+        key to Pair(left, right)
+    }.toMap()
+}
+
+
+
 fun doPartA(lines: List<String>): Int {
-return -1
+    return -1
 }
 
 
 fun doPartB(lines: List<String>): Long {
-return -1
+    return -1
 
 }
 
--- src/test/kotlin/com/shawnbutton/advent2023/day08/Day08Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day08/Day08Test.kt
@@ -6,7 +6,39 @@ import kotlin.test.Ignore
 
 class Day08Test {
 
-  val lines = listOf("A", "B", "C")
+
+    val lines = listOf(
+        "RL",
+        "",
+        "AAA = (BBB, CCC)",
+        "BBB = (DDD, EEE)",
+        "CCC = (ZZZ, GGG)",
+        "DDD = (DDD, DDD)",
+        "EEE = (EEE, EEE)",
+        "GGG = (GGG, GGG)",
+        "ZZZ = (ZZZ, ZZZ)"
+    )
+
+    @Test
+    fun `should get left right instructions`() {
+        val leftRight = getIntructions(lines)
+        assertEquals("RL", leftRight)
+    }
+
+    @Test
+    fun `should get map of directions`() {
+        val directions = getDirections(lines)
+        val expected = mapOf(
+            "AAA" to Pair("BBB", "CCC"),
+            "BBB" to Pair("DDD", "EEE"),
+            "CCC" to Pair("ZZZ", "GGG"),
+            "DDD" to Pair("DDD", "DDD"),
+            "EEE" to Pair("EEE", "EEE"),
+            "GGG" to Pair("GGG", "GGG"),
+            "ZZZ" to Pair("ZZZ", "ZZZ")
+        )
+        assertEquals(expected, directions)
+    }
 
     @Test
     @Ignore

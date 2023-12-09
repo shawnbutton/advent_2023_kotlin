395c8b965846e8459dd9e5b49d0e9acd95bb83b7
shawn <shawnbutton@gmail.com>
2023-12-08 23:58:06 -0500

    ignore day 8 tests for now

--- src/test/kotlin/com/shawnbutton/advent2023/day08/Day08Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day08/Day08Test.kt
@@ -2,18 +2,21 @@ package com.shawnbutton.advent2023.day08
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
 class Day08Test {
 
   val lines = listOf("A", "B", "C")
 
     @Test
+    @Ignore
     fun `do part a`() {
         val result = doPartA(lines)
         assertEquals(6440, result)
     }
 
     @Test
+    @Ignore
     fun `do part b`() {
         val result = doPartB(lines)
         assertEquals(5905, result)

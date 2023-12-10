9ea9224904ae93062269603bf49f2c0fdeb23c76
shawn <shawnbutton@gmail.com>
2023-12-09 23:27:02 -0500

    prepare for day 9

---
+++ src/main/kotlin/com/shawnbutton/advent2023/day09/Day09.kt
@@ -0,0 +1,22 @@
+package com.shawnbutton.advent2023.day09
+
+import com.shawnbutton.advent2023.loadFile
+
+
+fun doPartA(lines: List<String>): Int {
+    return -1
+}
+
+fun doPartB(lines: List<String>): Int {
+    return -1
+}
+
+fun main() {
+    val lines = loadFile("/day08.txt")
+
+    print(doPartA(lines))
+    print("\n")
+
+    print(doPartB(lines))
+    print("\n")
+}
---
+++ src/test/kotlin/com/shawnbutton/advent2023/day09/Day09Test.kt
@@ -0,0 +1,13 @@
+package com.shawnbutton.advent2023.day09
+
+import com.shawnbutton.advent2023.loadFile
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day09Test {
+    @Test
+    fun doPartA() {
+//        val lines = loadFile("/day09.txt")
+//        assertEquals(542529149, doPartA(lines))
+    }
+}

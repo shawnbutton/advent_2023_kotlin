33313778bf33c7b3ec33fb72923e1f64e4c0aa20
shawn <shawnbutton@gmail.com>
2023-12-10 20:08:53 -0500

    get the derived sequences

--- src/main/kotlin/com/shawnbutton/advent2023/day09/Day09.kt
+++ src/main/kotlin/com/shawnbutton/advent2023/day09/Day09.kt
@@ -6,6 +6,27 @@ fun parseHistory(line1: String): List<Int> {
     return line1.split(" ").map { it.toInt() }
 }
 
+fun getChangeSequence(values: List<Int>): List<Int> {
+    val changeSequence = mutableListOf<Int>()
+    for (i in 1 until values.size) {
+        changeSequence.add(values[i] - values[i - 1])
+    }
+    return changeSequence
+}
+
+fun getChangeSequencyRecursively(values: List<Int>): List<List<Int>> {
+    var changeSequence = values
+
+    val results = mutableListOf<List<Int>>(values)
+    while (!changeSequence.all { it == 0 }) {
+        changeSequence = getChangeSequence(changeSequence)
+        results.add(changeSequence)
+    }
+
+    return results
+}
+
+
 fun doPartA(lines: List<String>): Int {
     return -1
 }
--- src/test/kotlin/com/shawnbutton/advent2023/day09/Day09Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2023/day09/Day09Test.kt
@@ -1,6 +1,5 @@
 package com.shawnbutton.advent2023.day09
 
-import com.shawnbutton.advent2023.loadFile
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -10,13 +9,30 @@ class Day09Test {
     val line2 = "1 3 6 10 15 21"
     val line3 = "10 13 16 21 30 45"
 
-
-
     @Test
     fun `should parse history line`() {
         assertEquals(listOf(0, 3, 6, 9, 12, 15), parseHistory(line1))
     }
 
+    @Test
+    fun `should get change sequence`() {
+        val values = parseHistory(line1)
+
+        assertEquals(listOf(3, 3, 3, 3, 3), getChangeSequence(values))
+    }
+
+    @Test
+    fun `should recursively get change sequence`() {
+        val values = parseHistory(line1)
+
+        val expected = listOf(
+            listOf(0, 3, 6, 9, 12, 15),
+            listOf(3, 3, 3, 3, 3),
+            listOf(0, 0, 0, 0)
+        )
+
+        assertEquals(expected, getChangeSequencyRecursively(values))
+    }
 
     @Test
     fun doPartA() {

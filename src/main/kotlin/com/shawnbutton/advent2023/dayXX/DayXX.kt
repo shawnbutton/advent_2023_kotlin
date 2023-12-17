1dd531974436f1dc867b22506c581caa4f455878
shawn <shawnbutton@gmail.com>
2023-12-17 13:16:19 -0500

    fix template

---
+++ src/main/kotlin/com/shawnbutton/advent2023/dayXX/DayXX.kt
@@ -0,0 +1,21 @@
+package com.shawnbutton.advent2023.dayXX
+
+import com.shawnbutton.advent2023.loadFile
+
+fun doPartA(lines: List<String>): Int {
+    return -1
+}
+
+fun doPartB(lines: List<String>): Int {
+    return -1
+}
+
+fun main() {
+    val lines = loadFile("/day10.txt")
+
+    print(doPartA(lines))
+    print("\n")
+
+    print(doPartB(lines))
+    print("\n")
+}

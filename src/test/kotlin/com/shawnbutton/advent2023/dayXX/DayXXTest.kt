9d7ee73d9ebd0598b36fe8b66dce69d56866605f
shawn <shawnbutton@gmail.com>
2023-12-17 12:33:05 -0500

    make a day template

---
+++ src/test/kotlin/com/shawnbutton/advent2023/dayXX/DayXXTest.kt
@@ -0,0 +1,20 @@
+package com.shawnbutton.advent2023.dayXX
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DayXXTest {
+
+    val lines = listOf("")
+
+    @Test
+    fun doPartA() {
+        assertEquals(-1, doPartA(lines))
+    }
+
+    @Test
+    fun doPartB() {
+
+        assertEquals(-1, doPartA(lines))
+    }
+}
